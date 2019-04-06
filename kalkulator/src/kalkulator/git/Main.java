cf3d7984d00ec9ff7e3f19b4ef6828956a056555
Grzegorz Rogoziński <norogoz@gmail.com>
2019-04-06 21:59:40 +0200

    Initial commit

---
+++ kalkulator/src/kalkulator/git/Main.java
@@ -0,0 +1,10 @@
+package kalkulator.git;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Java Project");
+
+	}
+
+}
---
+++ kalkulator/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Unquadhexium
+ *
+ */
+module kalkulator {
+}
